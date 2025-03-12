3c149e9dbacc737ada7f4b763b1e379a92ed0b20
Arthur Gabriel <122728865+arthur-Gsilva@users.noreply.github.com>
2025-03-12 13:05:06 -0300

    primeiro passo para sessoes e responsividade

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,47 @@ window.addEventListener('scroll', () => {
         header.classList.remove('background-header')
     }
 })
+
+
+// WORKERS LIST
+
+const workersContainer = document.querySelector(".workers-items");
+
+function renderWorkers() {
+    workers.forEach(worker => {
+        const workerDiv = document.createElement("div");
+        workerDiv.classList.add("worker-item");
+
+        workerDiv.innerHTML = `
+            <div class="worker-img" style="background-image: url('${worker.image}');"></div>
+            <h4>${worker.name}</h4>
+            <h5>${worker.role}</h5>
+            <div class="worker-social">
+                <a href="${worker.linkedin}" target="_blank" class="linkedin-icon">
+                    <i class='bx bxl-linkedin-square'></i>
+                </a>
+                <a href="${worker.github}" target="_blank" class="github-icon">
+                    <i class='bx bxl-github'></i>
+                </a>
+            </div>
+        `;
+
+        workersContainer.appendChild(workerDiv);
+    });
+}
+
+renderWorkers();
+
+
+// SCROLL REVEAL
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+})
+
+sr.reveal('.img-banner, .how-it-item', {origin: 'left'})
+sr.reveal('.banner-title, .where-it-item', {origin: 'right'})
+sr.reveal('.section3-items, .workers-items', { interval: 100 })
---
+++ assets/js/workers.js
@@ -0,0 +1,45 @@
+const workers = [
+    {
+        name: 'Itoshi Rin',
+        role: 'Front-end',
+        linkedin: 'https://www.linkedin.com/in/arthurgabrielsilva/',
+        image: 'https://i.pinimg.com/736x/53/c7/a1/53c7a1aa078b64a265f0eb7e287f938b.jpg',
+        github: 'https://github.com/arthur-Gsilva'
+    },
+    {
+        name: 'Meguru Bachira',
+        role: 'Back-end',
+        linkedin: 'https://www.linkedin.com/in/arthurgabrielsilva/',
+        image: 'https://i.pinimg.com/736x/90/c8/34/90c8347f1ffe0035130526eda74048d9.jpg',
+        github: 'https://github.com/arthur-Gsilva'
+    },
+    {
+        name: 'Felipe Silva',
+        role: 'Front-end',
+        linkedin: 'https://www.linkedin.com/in/arthurgabrielsilva/',
+        image: 'https://s2-extra.glbimg.com/SQZbAPDNplnD-6Kd0HecTLjaXKI=/0x0:1170x1165/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_1f551ea7087a47f39ead75f64041559a/internal_photos/bs/2023/F/T/qwUKRyQ4mn2osqsUu4xQ/whatsapp-image-2023-12-22-at-10.27.23-am.jpeg',
+        github: 'https://github.com/arthur-Gsilva'
+    },
+    {
+        name: 'Michael Kaiser',
+        role: 'Gestão',
+        linkedin: 'https://www.linkedin.com/in/arthurgabrielsilva/',
+        image: 'https://i.pinimg.com/736x/e6/33/18/e63318b1296b7ab9e87297368e014a23.jpg',
+        github: 'https://github.com/arthur-Gsilva'
+    },
+    {
+        name: 'José Carlos',
+        role: 'Front-end',
+        linkedin: 'https://www.linkedin.com/in/arthurgabrielsilva/',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNRjhGcIcfxWzHB3z8jEAfXSJIEeOHv4fw4Q&s',
+        github: 'https://github.com/arthur-Gsilva'
+    },
+    {
+        name: 'Wendell Carvalho',
+        role: 'Documentação',
+        linkedin: 'https://www.linkedin.com/in/arthurgabrielsilva/',
+        image: 'https://wendellcarvalho.com.br/wp-content/uploads/2023/07/pessoa-intensa.jpg',
+        github: 'https://github.com/arthur-Gsilva'
+    },
+
+]
